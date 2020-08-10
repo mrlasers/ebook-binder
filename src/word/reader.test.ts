766ff5e031c9eb33b2c5322d52ebdeb5ad64e153
Timothy Pew <tim@timothypew.com>
2020-08-09 18:30:05 -0700

    initial commit
    basic xml parser working with tests

---
+++ src/word/reader.test.ts
@@ -0,0 +1,44 @@
+import * as Reader from './reader'
+
+it('parses <hello style="cool"><world>Earth</world></hello>', () => {
+  const input = '<hello style="cool"><world>Earth</world></hello>'
+  const expected = {
+    type: 'node',
+    name: 'hello',
+    attributes: {
+      style: 'cool'
+    },
+    children: [
+      {
+        type: 'node',
+        name: 'world',
+        attributes: {},
+        children: [
+          {
+            type: 'text',
+            text: 'Earth'
+          }
+        ]
+      }
+    ]
+  }
+
+  // ** goal for test
+  // const input = '<hello><world>Earth</world></hello>'
+  // const expected = {
+  //   type: 'node',
+  //   name: 'hello',
+  //   attributes: {},
+  //   children: [
+  //     {
+  //       type: 'node',
+  //       name: 'world',
+  //       attributes: {},
+  //       children: [{ type: 'text', text: 'Earth' }]
+  //     }
+  //   ]
+  // }
+
+  expect.assertions(1)
+  return expect(Reader.parse(input)).resolves.toEqual(expected)
+})
---
+++ src/word/reader.ts
@@ -0,0 +1,136 @@
+import * as Sax from 'sax'
+
+// export function parse () {
+//   const parser = Sax.parser(true)
+
+//   let node = {}
+
+//   parser.onopentag = function({ name, attributes }) {
+//     node = { name }
+//   }
+
+//   parser.onend
+
+//   return node
+// }
+
+export function parse (xml: string) {
+  return new Promise((resolve, reject) => {
+    const parser = Sax.parser(true)
+
+    const root = { children: [] }
+    const stack = []
+    let currentNode = root
+
+    parser.onopentag = function ({ name, attributes }) {
+      console.log('open (currentNode):', currentNode)
+
+      const newNode = {
+        type: 'node',
+        name,
+        attributes,
+        children: []
+      }
+
+      currentNode.children.push(newNode)
+      stack.push(currentNode)
+      currentNode = newNode
+    }
+
+    parser.onclosetag = function () {
+      currentNode = stack.pop()
+    }
+
+    parser.onend = function () {
+      resolve(root.children[0])
+    }
+
+    parser.ontext = function (text) {
+      const newNode = {
+        type: 'text',
+        text
+      }
+
+      currentNode.children.push(newNode)
+    }
+
+    parser.onerror = function (e) {
+      console.error('we have a problem!!!')
+      reject()
+    }
+
+    parser.write(xml).close()
+  })
+}
+
+// interface Text {
+//   type: 'text'
+//   text: string
+// }
+
+// interface Block {
+//   type: 'node'
+//   name: string
+//   attributes?: object
+//   children: (Block | Text)[]
+// }
+
+// type Node = Text | Block
+
+// interface CreateNodeParams {
+//   name?: string
+//   text?: string
+//   attributes: object
+//   children: (Block | Text)[]
+// }
+
+// export function createTextNode (text): Text {
+//   return {
+//     type: 'text',
+//     text
+//   }
+// }
+
+// export function createBlockNode ({
+//   name,
+//   attributes = {},
+//   children = []
+// }): Block {
+//   return {
+//     type: 'node',
+//     name,
+//     attributes,
+//     children
+//   }
+// }
+
+// export function createNode ({
+//   name,
+//   text,
+//   attributes,
+//   children
+// }: CreateNodeParams): Node {
+//   if (text) {
+//     return {
+//       type: 'text',
+//       text
+//     }
+//   }
+
+//   return {
+//     type: 'node',
+//     name,
+//     attributes: {},
+//     children: []
+//   }
+// }
+
+// export function xml (xml: string) {
+//   const parser = Sax.parser(true)
+
+//   const stack = []
+
+//   parser.onopentag = function (node) {}
+
+//   return {}
+// }
