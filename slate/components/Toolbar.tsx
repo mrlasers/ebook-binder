93d336cafdf5e3f4e4409b6f3cb5b88bad848e0c
Timothy Pew <tim@timothypew.com>
2020-08-12 22:05:47 -0700

    add toolbar component

---
+++ slate/components/Toolbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Toolbar = ({ ...props }) => {
+  return (
+    <nav>
+      <button>Home</button>
+      <button>New Book</button>
+      <button>Add Document</button>
+    </nav>
+  )
+}
--- slate/index.tsx
+++ slate/index.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useCallback, useMemo, useState } from 'react'
 import * as ReactDOM from 'react-dom'
 
 import Editor from './Editor/index'
+import { Toolbar } from './components'
 
 const App = () => {
   return (
     <div>
+      <Toolbar />
       <Editor onChange={(_) => _} />
     </div>
   )
